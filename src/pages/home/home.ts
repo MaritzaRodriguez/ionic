c8407b19692fec7ffe5e3757ec9cd11593b1068b
MaritzaRodriguez <maritza.rodriguez.enc@gmail.com>
2018-06-12 16:40:06 -0500

    formularios

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  validarFormulario(formulario: NgForm){
+    console.log(formulario);
   }
 
 }
